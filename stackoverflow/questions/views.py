d4c536ba54bfb5f6b3d493cf46d539f8b3e65aa7
AKXAT <akshat.aksharma@gmail.com>
2021-12-13 13:13:28 +0530

    Added feature to view the questions from Stackoverflow

--- stackoverflow/questions/views.py
+++ stackoverflow/questions/views.py
@@ -1,3 +1,14 @@
+from django.conf.urls import url
 from django.shortcuts import render
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def questions(request):
+    question = 'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow'
+    r = requests.get(question)
+    s = r.json()
+    return render(request,'base.html',{'s':s})
 
 # Create your views here.
