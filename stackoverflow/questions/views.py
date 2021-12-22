fced58f34106daba7cdd6da728ef1b681d8a423f
AKXAT <akshat.aksharma@gmail.com>
2021-12-22 23:37:00 +0530

    Added search option for questions

--- stackoverflow/questions/views.py
+++ stackoverflow/questions/views.py
@@ -4,11 +4,19 @@ import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def questions(request):
-    question = 'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow'
-    r = requests.get(question)
-    s = r.json()
-    return render(request,'base.html',{'s':s})
+
+# @api_view()
+# def questions(request):
+#     question = 'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow'
+#     r = requests.get(question)
+#     s = r.json()
+#     return render(request,'base.html',{'s':s})
 
 # Create your views here.
+
+def questions(request):
+    qus = request.GET.get('qus')
+    url = f'https://api.stackexchange.com/2.3/search?intitle={qus}&site=stackoverflow'
+    r = requests.get(url)
+    s = r.json()
+    return render(request,'base.html',{'s':s})
